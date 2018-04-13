26ab701efe2a1905c8f03c0b7b813ac18f89dc1d
Marco Cogoni <cogoni@gmail.com>
2018-04-13 13:18:08 +0200

    it should be working, but still some serial glitches...

--- fsas.py
+++ fsas.py
@@ -4,8 +4,32 @@ import numpy as np
 import pickle
 from datetime import datetime
 import matplotlib.pyplot as plt
+import os.path
 
-debug_flag = False
+debug_flag = True
+
+filename = None
+
+if len(sys.argv) == 1:
+    print "---------------------------"
+    print "--- FSAS TRACEDUMP TOOL ---"
+    print "--- Marco Cogoni IS0KYB ---"
+    print "---------------------------"
+    print
+    print "Command line options:"
+    print "-f tracedump.pickle\t -> load a saved tracedump from File and process it producing an out image and print FSAS data"
+    print "-c\t\t\t -> use the GPIB bus to Capture a trace dump from the FSAS\n"
+    exit()
+elif len(sys.argv) == 2:
+    if sys.argv[1] == "-c":
+        pass
+elif len(sys.argv) == 3:
+    if sys.argv[1] == "-f":
+        filename = sys.argv[2]
+        with open(filename, "rb") as fn:
+            dump_data = pickle.load(fn)
+            print "--- LOADING FILE TO PROCESS ---"
+
 
 TRACE_SIZE = 2190 # FSAS dumps 2190 bytes when asked for a trace complete with the header (the first 1802 bytes for 901 points (one int16 each))
 
@@ -24,16 +48,20 @@ smcp_index = 1     # Rohde-Schwarz Signal Generator 5kHz-1.3GHz
 serial_ports = ["/dev/ttyUSB0", "/dev/ttyUSB1"]
 
 serial_open_flag = False
-for p in serial_ports:
-    s = serial.Serial(p, 115200)
-    if s.isOpen()
-        serial_open_flag = True
-        print "Serial connection active"
-        break
-if not serial_open_flag:
-    print "Serial connection not working!!!"
-    exit()
-    
+
+if not filename:
+    for p in serial_ports:
+        if os.path.exists(p):
+            #s = serial.Serial(port=p, baudrate=115200, bytesize=8, parity='N', stopbits=1, timeout=None, xonxoff=0, rtscts=0)
+            s = serial.Serial(port=p, baudrate=115200)
+            if s.isOpen():
+                serial_open_flag = True
+                print "Serial connection active:", p
+                break
+    if not serial_open_flag:
+        print "Serial connection not working!!!"
+        exit()
+        
 
 gpib_buffer_file = "GPIB_tracedump.%s.pickle" % str(datetime.now()).split(".")[0].replace(" ", "_")
 
@@ -52,7 +80,8 @@ def query(cmd):
     if debug_flag:
         print string
     time.sleep(GPIB_delay)
-    print s.readline()
+    if s.inWaiting():
+        print s.readline()
     
 # obtain the talker or listener index from a specific address as specified in the GPIB addressing table
 def get_listener(index):
@@ -71,41 +100,45 @@ def set_listener_talker(listener, talker):
 #########################################    
 # Start writing on the serial interface #
 #########################################
+if not filename:
+    while s.inWaiting():
+        s.read(1)
+    print "Empty the serial queue"
+    query("E0") # disable local serial echo
+    query("R") # enable remote operations on the GPIB bus
 
-query("E0") # disable local serial echo
-query("R") # enable remote operations on the GPIB bus
+    # switch FSAS to listen commands and control inteface to talk
+    set_listener_talker(fsas_index, control_index)
 
-# switch FSAS to listen commands and control inteface to talk
-set_listener_talker(fsas_index, control_index)
+    query("DF:C 1M") # set the center frequency on the FSAS
+    query('DTRACE:BLOCK:T_1?') # ask for a trace (1) dump
 
-query("DF:C 1M") # set the center frequency on the FSAS
-query('DTRACE:BLOCK:T_1?') # ask for a trace (1) dump
+    # switch control interface to listen to commands and FSAS to talk
+    set_listener_talker(control_index, fsas_index)
 
-# switch control interface to listen to commands and FSAS to talk
-set_listener_talker(control_index, fsas_index)
+    print "---- NOW RECEIVING TRACE DUMP DATA FROM FSAS ----"
 
-print "---- NOW RECEIVING TRACE DUMP DATA FROM FSAS ----"
+    buffer = ""
+    while len(buffer) < TRACE_SIZE + 10: # we add some margin since we're adding control strings
+        tmp = ""
+        write("Y") # read a full buffer of data from the instrument
+        time.sleep(GPIB_delay) # wait a bit for the data transmission to finish
+        tmp += "XXX" # we add a marker to locate different dumps (~395 bytes each)
+        while s.inWaiting()>0: # is any character present in the serial buffer
+            tmp += s.read(1) # read one byte
+        buffer += tmp
 
-buffer = ""
-while len(buffer) < TRACE_SIZE + 100: # we add some margin since we're adding control strings
-    tmp = ""
-    write("Y") # read a full buffer of data from the instrument
-    time.sleep(GPIB_delay) # wait a bit for the data transmission to finish
-    tmp += "XXX" # we add a marker to locate different dumps (~395 bytes each)
-    while s.inWaiting()>0: # is any character present in the serial buffer
-        tmp += s.read(1) # read one byte
-    buffer += tmp
+    # remove garbage
+    buffer = buffer.replace("XXXOK\r\nOK\r\n\xfe", "").replace("XXX\xfe", "")
 
-# remove garbage
-buffer = buffer.replace("XXXOK\r\nOK\r\n\xfe", "").replace("XXX\xfe", "")
-
-# Save binary buffer to disk
-with open(gpib_buffer_file, 'wb') as f:
-    pickle.dump(buffer,f)
-
+    # Save binary buffer to disk
+    with open(gpib_buffer_file, 'wb') as f:
+        pickle.dump(buffer,f)
+else:
+    buffer = dump_data
 
 # let's decode the uncorrected trace information
-raw_trace_data = np.right_shift(np.fromstring(buffer[:], dtype='<i2', count=901), 4)
+raw_trace_data = np.right_shift(np.fromstring(buffer[:], dtype=np.uint16, count=901), 4)
 
 # let's decode the header information: we don't need everything!
 
@@ -125,14 +158,14 @@ data_values_dict = {}
 # read in the values
 for off in data_type_dict:
     data_values_dict[off] = np.fromstring(buffer[off:], dtype=data_type_dict[off], count=1)
-    print off, data_values_dict[off],"\t\t", data_descript_dict[off]
+    print off, data_values_dict[off][0],"\t\t", data_descript_dict[off]
 
     
 ############ Visualization
 font = {'weight' : 'bold',
-        'size'   : 18}
+        'size'   : 15}
 
-matplotlib.rc('font', **font)
+plt.rc('font', **font)
 
 fig = plt.figure(figsize=(16,16))
 
@@ -145,29 +178,29 @@ rf_att = np.fromstring(buffer[1813:], dtype=data_type_dict[1813], count=1)[0]
 ax = fig.add_subplot(111)#, axisbg='blue')
 ax.set_facecolor('blue')
 fig.patch.set_facecolor('blue')
-print ref_level
+
 trace_data = (raw_trace_data-3938.)/3938.*110+ref_level # FIXME: the range should be decoded from the header data too!
-ax.plot(np.linspace(start_freq, stop_freq, 901), trace_data[:], c='lightgreen')
-grid(color="w")
-xlabel("Freq. (MHz)", color="w", fontweight='bold')
-ylabel("Power (dBm)", color="w", fontweight='bold')
-ylim(-110+ref_level, ref_level)
-xlim(start_freq, stop_freq)
-xticks(np.linspace(start_freq, stop_freq, 11), color="w")
-yticks(np.linspace(-110+ref_level, ref_level, 12), color="w")
-title("ROHDE & SCHWARZ FSAS", color='w', fontsize=20)
+plt.plot(np.linspace(start_freq, stop_freq, 901), trace_data[:], c='lightgreen')
+plt.grid(color="w")
+plt.xlabel("Freq. (MHz)", color="w", fontweight='bold')
+plt.ylabel("Power (dBm)", color="w", fontweight='bold')
+plt.ylim(-110+ref_level, ref_level)
+plt.xlim(start_freq, stop_freq)
+plt.xticks(np.linspace(start_freq, stop_freq, 11), color="w")
+plt.yticks(np.linspace(-110+ref_level, ref_level, 12), color="w")
+plt.title("ROHDE & SCHWARZ FSAS", color='w', fontsize=20)
 rbw_text = "RBW: "+str(rbw)+" kHz"
 rf_att_text = "RF Att: "+str(rf_att)+" dB"
 sweep_text = "Sweep: "+str(sweep_time)+" s"
-ax.text(0.8,1.08, rbw_text, size=15,
+plt.text(0.8,1.08, rbw_text,
      horizontalalignment='left',
      verticalalignment='center', transform=ax.transAxes, color="w")
-ax.text(0.8,1.06, rf_att_text,
+plt.text(0.8,1.06, rf_att_text,
      horizontalalignment='left',
      verticalalignment='center', transform=ax.transAxes, color="w")
-ax.text(0.8,1.04, sweep_text,
+plt.text(0.8,1.04, sweep_text,
      horizontalalignment='left',
      verticalalignment='center', transform=ax.transAxes, color="w")
 
-fig.savefig(gpib_buffer_file[:-7] + ".jpg")
+fig.savefig(gpib_buffer_file[:-7] + ".png", frameon=True, facecolor='b', edgecolor='b',)
 
